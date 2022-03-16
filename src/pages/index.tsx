d044471f989d603335a8e5d7c2cf38c5ebf43105
jvkass <jvcnmata@gmail.com>
2022-03-15 21:19:52 -0300

    Consumindo API do Stripe(SSR - Server Side Rendering)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
+  console.log(props);
+
   return (
     <>
       <Head>
@@ -16,13 +26,36 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+//Obs: sempre criar essa função async com o nome getServerSideProps
+export const getServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KdjvtCYpOzohnirpSPY7yo8'
+  // para pegar informações extrar, como nome do produto do Stripe
+  // , {
+  //   expand: ['product']    
+  // }  
+  )
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product
+    }
+  }
 }
