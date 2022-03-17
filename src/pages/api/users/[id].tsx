256f2cb6cdc804a8d97e4b2c060f9e7ca25acb93
jvkass <jvcnmata@gmail.com>
2022-03-17 18:01:37 -0300

    Pegando o params da aplicação

---
+++ src/pages/api/users/[id].tsx
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    console.log(request.query)
+    
+    const users = [
+        { id:1, name:'João'},
+        { id:2, name:'Vitor'},
+        { id:3, name:'Coelho'},
+    ]
+    return response.json(users)
+}
