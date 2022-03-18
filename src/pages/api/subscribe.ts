7442339608a2f5dcc3ecd3e5d611522709149003
jvkass <jvcnmata@gmail.com>
2022-03-18 14:26:39 -0300

    Webhooks do Stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -29,19 +29,19 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
         let customerId = user.data.stripe_customer_id
 
-        if (!customerId){
+        if (!customerId) {
             const stripeCustomer = await stripe.customers.create({
                 email: session.user.email
             })
-    
+
             await fauna.query(
                 query.Update(
-                    query.Ref(query.Collection('users'),user.ref.id),
+                    query.Ref(query.Collection('users'), user.ref.id),
                     {
                         data: {
                             stripe_customer_id: stripeCustomer.id
                         }
-                    } 
+                    }
                 )
             )
             customerId = stripeCustomer.id;
@@ -60,7 +60,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             cancel_url: process.env.STRIPE_CANCEL_URL
         })
 
-        return res.status(200).json({sessionId: stripeCheckoutSession.id})
+        return res.status(200).json({ sessionId: stripeCheckoutSession.id })
     } else {
         res.setHeader('allow', 'POST')
         res.status(405).end('Method not allowed')
