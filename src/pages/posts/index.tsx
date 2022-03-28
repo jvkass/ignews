81717d55fa4329fcdffb236b488238497138ad7e
jvkass <jvcnmata@gmail.com>
2022-03-28 19:57:11 -0300

    Página Post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,32 @@
-
+import Head from 'next/head';
+import styles from './styles.module.scss';
 
 export default function Posts() {
   return (
     <>
-        <h1>Post</h1>
+      <Head>
+        <title>Post | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a>
+            <time>28 de março de 2021</time>
+            <strong>Creating Post</strong>
+            <p>Information for paragraph post</p>
+          </a>
+          <a>
+            <time>28 de março de 2021</time>
+            <strong>Creating Post 2</strong>
+            <p>Information for paragraph post</p>
+          </a>
+          <a>
+            <time>28 de março de 2021</time>
+            <strong>Creating Post 3</strong>
+            <p>Information for paragraph post</p>
+          </a>
+        </div>
+      </main>
     </>
   );
 }
